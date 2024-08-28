0f49e50941813dbf4097a16666bbebd587c987cd
GEZMEDIA <zimpitagervasio@gmail.com>
2024-08-28 08:24:32 -0800

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -3,16 +3,46 @@
 // variables
 
 let menuEl = document.querySelector('.js-menu-btn');
-let menuBarEL = document.querySelector('.nav-bar');
+let menuBarEL = document.querySelector('.nav-bar ul');
 let iconEl = document.querySelector('.menu button i')
 
 // Add event listener when the menu button is clicked
 menuEl.addEventListener('click', ()=>{
     // toggle the menu bar
-    menuBarEL.classList.toggle("show");
+    menuBarEL.classList.toggle("active");
     if(iconEl.classList.contains('fa-bars')){
         return iconEl.classList.replace('fa-bars', 'fa-times')
     }else{
         return iconEl.classList.replace('fa-times', 'fa-bars')
     }
 });
+
+// Close the nav after click
+// variables
+
+let navListEl = document.querySelectorAll('.nav-list');
+
+navListEl.forEach(nav =>{
+    nav.addEventListener('click', ()=>{
+        menuBarEL.classList.remove("active");
+        if(iconEl.classList.contains('fa-times')){
+            return iconEl.classList.replace('fa-times', 'fa-bars');
+        }
+    })
+})
+
+
+// Floating button
+const floatingBtnEl = document.querySelector('.floating-btn');
+
+window.addEventListener('scroll', ()=>{
+    if(scrollY > 20){
+        floatingBtnEl.style.display = 'block';
+    }else{
+        floatingBtnEl.style.display = 'none';
+    }
+})
+
+floatingBtnEl.addEventListener('click', ()=>{
+    window.scrollY = 0;
+})
