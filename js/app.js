a229b48bfbac20d806525ec729b46240239aae13
GEZMEDIA <zimpitagervasio@gmail.com>
2024-08-30 06:41:47 -0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -31,3 +31,58 @@ const typeEffect = () => {
 }
 
 typeEffect();
+
+
+// Scroll animations
+
+// custom scroll reveal animation
+
+const scr = ScrollReveal({
+    distance : '65px',
+    duration : 2600,
+    delay :450,
+    reset : true
+
+});
+
+// Animation on DOM Reload
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    scr.reveal('.hero-title', {delay:200, origin:'bottom'});
+
+})
+
+// Hero section
+scr.reveal('.hero-title', {delay:200, origin:'bottom'});
+scr.reveal('.left-section', {delay:100, origin:'bottom'});
+scr.reveal('.hero-section', {delay:80, origin:'bottom'});
+scr.reveal('.animate', {delay:100, origin:'bottom'});
+
+// about section
+// scr.reveal('.image-container', {delay:200, origin:'bottom'});
+// scr.reveal('.about-left-section', {delay:100, origin:'bottom'});
+// scr.reveal('.about-section', {delay:100, origin:'bottom'});
+
+// scr.reveal('.review-container', {delay:100, origin:'bottom'});
+// scr.reveal('.about-right-section', {delay:100, origin:'bottom'});
+// scr.reveal('.about-title', {delay:100, origin:'bottom'});
+// scr.reveal('.read-more-btn', {delay:100, origin:'bottom'});
+
+// Planning section
+
+// scr.reveal('.planning-section', {delay:100, origin:'bottom'});
+scr.reveal('.title', {delay:100, origin:'bottom'});
+// scr.reveal('.planning-container', {delay:100, origin:'bottom'});
+
+// contact section
+// scr.reveal('.contact', {delay:100, origin:'bottom'});
+// scr.reveal('.contact-title', {delay:100, origin:'bottom'});
+// scr.reveal('.contact-details-section', {delay:100, origin:'bottom'});
+// scr.reveal('.contact-description', {delay:100, origin:'bottom'});
+// scr.reveal('.contact-icons', {delay:100, origin:'bottom'});
+// scr.reveal('.contact-form-section', {delay:100, origin:'bottom'});
+
+// Services section
+// scr.reveal('.service-section', {delay:100, origin:'bottom'});
+scr.reveal('.we-do', {delay:100, origin:'bottom'});
+// scr.reveal('.card', {delay:100, origin:'bottom'});
