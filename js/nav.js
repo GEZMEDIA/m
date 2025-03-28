21720f04a948e05175b6c176dedc4d15c9dd0241
GEZMEDIA <gezmedia@gmail.com>
2025-03-28 08:50:00 +0200

    added blog 2 on the blog section

--- js/nav.js
+++ js/nav.js
@@ -2,19 +2,21 @@
 
 // variables
 
-let menuEl = document.querySelector('.js-menu-btn');
+let menuEl = document.querySelectorAll('.js-menu-btn');
 let menuBarEL = document.querySelector('.nav-bar ul');
-let iconEl = document.querySelector('.menu button i')
+let iconEl = document.querySelector('.menu button i');
 
 // Add event listener when the menu button is clicked
-menuEl.addEventListener('click', ()=>{
-    // toggle the menu bar
-    menuBarEL.classList.toggle("active");
-    if(iconEl.classList.contains('fa-bars')){
-        return iconEl.classList.replace('fa-bars', 'fa-times')
-    }else{
-        return iconEl.classList.replace('fa-times', 'fa-bars')
-    }
+menuEl.forEach(menu =>{
+    menu.addEventListener('click', ()=>{
+        // toggle the menu bar
+        menuBarEL.classList.toggle("active");
+        if(iconEl.classList.contains('fa-bars')){
+            return iconEl.classList.replace('fa-bars', 'fa-times')
+        }else{
+            return iconEl.classList.replace('fa-times', 'fa-bars')
+        }
+    });
 });
 
 // Close the nav after click
